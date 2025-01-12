cd739ca4438cad23a1ccdb17c4b2bcf51662777e
hemantrajurkar123 <hemant.rajurkar123@gmail.com>
2025-01-12 14:11:09 +0530

    conditional methods

---
+++ src/test/java/webdrivermethods/ConditionalMethodsDemo.java
@@ -0,0 +1,48 @@
+package webdrivermethods;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ConditionalMethodsDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/register");
+		driver.manage().window().maximize();
+		
+		//isDisplayd()
+		boolean status= driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']")).isDisplayed();
+		System.out.println(status);
+		
+		//isEnabled
+		boolean status1=driver.findElement(By.xpath("//input[@id='FirstName']")).isEnabled();
+		System.out.println(status1);
+		
+		//isSelected
+		System.out.println("Before Selection...");
+		WebElement male_radio= driver.findElement(By.xpath("//input[@id='gender-male']"));
+		WebElement female_radio= driver.findElement(By.xpath("//input[@id='gender-female']"));
+		System.out.println(male_radio.isSelected()); //false
+		System.out.println(female_radio.isSelected()); //false
+		
+		male_radio.click(); //select the male radio button
+		System.out.println("After Selection ...");
+		System.out.println(male_radio.isSelected());//true
+		System.out.println(female_radio.isSelected()); //false
+		
+
+
+	}
+
+}
+
+
+//conditional methods - access these commands thorugh WebElement
+//-----------------------
+//returns boolean value( true/false)
+//
+//isDisplayed() - we can check display status of the element. Any element ex. logos and other elements
+//isEnabled() - we can check enable/disable status of the element/operational element . ex. input boxes
+//isSelected() - we can use to check the element is selected or not. ex checkboxes and radio buttons
